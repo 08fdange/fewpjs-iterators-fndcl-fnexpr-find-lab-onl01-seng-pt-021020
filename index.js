c8b0781d7a9d88e4115ab1a9c05024c312f2c17c
08fdange <08fdange@gmail.com>
2020-08-25 19:44:00 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winYear
+  array.find(function(obj) {
+    if (obj.result === "W") {
+      winYear = obj.year;
+    }
+  })
+  return winYear;
+}
